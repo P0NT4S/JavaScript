39d0d21bd04df74f928903369388f3071c44514a
P0NT4S <166468856+P0NT4S@users.noreply.github.com>
2024-05-24 16:27:13 -0300

    update

--- Projeto1-ListaDeTarefas/script.js
+++ Projeto1-ListaDeTarefas/script.js
@@ -1,14 +1,27 @@
 let entrada = document.getElementById('digTarefa');
 let tarefas = document.getElementById('tarefas');
 let botao = document.getElementById('botao');
+let feito = false
+let novaTarefa = document.createElement('p');
 
-function addTarefa(){
+function addTarefa() {
     if (entrada.value != '') {
-        var novaTarefa = document.createElement('p');
         novaTarefa.innerText = '- ' + entrada.value;
         tarefas.appendChild(novaTarefa);
         entrada.value = '';
     }
 }
 
+function marcaTarefa() {
+    if (feito == false) {
+        novaTarefa.style.textDecoration = 'line-through';
+        feito = true;
+    }
+    else {
+        novaTarefa.style.textDecoration = 'none';
+        feito = false;
+    }
+}
+
 botao.addEventListener('click', addTarefa)
+novaTarefa.addEventListener('click', marcaTarefa)
